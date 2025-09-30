038845fe670bffd167a2e6ea183555e5e735265a
Sadisms <dmitriy.stepanenko.2011@mail.ru>
2025-09-30 13:12:31 +0600

    feat: Add environment

--- elk_logger/logger.py
+++ elk_logger/logger.py
@@ -1,12 +1,22 @@
 import logging
 import sys
-from typing import Optional
+from typing import Literal, Optional
 
 import colorlog
 from logstash_async.handler import AsynchronousLogstashHandler
 
 
 _loggers = {}
+
+
+class EnvironmentFilter(logging.Filter):
+    def __init__(self, environment: str):
+        super().__init__()
+        self.environment = environment
+
+    def filter(self, record):
+        record.environment = self.environment
+        return True
 
 
 def setup_logger(
@@ -16,6 +26,7 @@ def setup_logger(
     logstash_port: int = 5959,
     enable_stdout: bool = True,
     enable_logstash: bool = True,
+    environment: Literal["dev", "prod", "staging", "test"] = "dev",
 ) -> logging.Logger:
     if name in _loggers:
         return _loggers[name]
@@ -27,11 +38,14 @@ def setup_logger(
     if logger.hasHandlers():
         logger.handlers.clear()
 
+    env_filter = EnvironmentFilter(environment)
+    logger.addFilter(env_filter)
+
     if enable_stdout:
         console_handler = colorlog.StreamHandler(sys.stdout)
         console_handler.setLevel(level)
         console_formatter = colorlog.ColoredFormatter(
-            "%(log_color)s%(asctime)s %(levelname)-8s%(reset)s %(blue)s[%(name)s]%(reset)s %(message)s",
+            "%(log_color)s%(asctime)s %(levelname)-8s%(reset)s %(purple)s[%(environment)s]%(reset)s %(blue)s[%(name)s]%(reset)s %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
             log_colors={
                 "DEBUG": "cyan",
@@ -49,6 +63,7 @@ def setup_logger(
             host=logstash_host,
             port=logstash_port,
             database_path=None,
+            extra_fields={"environment": environment},
         )
         logstash_handler.setLevel(level)
         logger.addHandler(logstash_handler)
