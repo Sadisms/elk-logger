e0abf093535ed03984a2eb33b3644019064607f5
Sadisms <dmitriy.stepanenko.2011@mail.ru>
2025-10-06 16:48:03 +0600

    feat: Expose new utilities for handling large data and JSON extraction in logger

--- elk_logger/__init__.py
+++ elk_logger/__init__.py
@@ -1,4 +1,14 @@
-from elk_logger.logger import get_logger, setup_logger
+from elk_logger.logger import (
+    get_logger, 
+    setup_logger, 
+    truncate_large_data, 
+    get_extra_from_json
+)
 
-__all__ = ["get_logger", "setup_logger"]
+__all__ = [
+    "get_logger", 
+    "setup_logger", 
+    "truncate_large_data", 
+    "get_extra_from_json"
+]
 __version__ = "0.1.0"
